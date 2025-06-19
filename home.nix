d996952f3a9cc78dbb68f1101446f96776dbd0e9
Kosuke Takami <kosuke.506@studist.jp>
2025-06-19 17:55:28 +0900

    Add claude

--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ lib, config, pkgs, ... }:
 
 {
   home.username = "kosuke.506";
@@ -12,6 +12,11 @@
   # want to update the value, then make sure to first check the Home Manager
   # release notes.
   home.stateVersion = "25.05"; # Please read the comment before changing.
+
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) [
+      "claude-code"
+  ];
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
@@ -29,6 +34,7 @@
     pkgs.ripgrep
     pkgs.nodejs
     pkgs.gnupg
+    pkgs.claude-code
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
